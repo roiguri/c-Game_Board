b384ddbb9608aef32b53c8c515298e08d75e4134
roiguri <155663297+roiguri@users.noreply.github.com>
2025-04-18 11:53:50 +0300

    Restructure chase algorithm class

--- src/algo/chase_algorithm.cpp
+++ src/algo/chase_algorithm.cpp
@@ -14,6 +14,50 @@ Action ChaseAlgorithm::getNextAction(
     const Tank& enemyTank,
     const std::vector<Shell>& shells
 ) {
-    // TODO: not implemented
+    // TODO: Will be implemented in a later commit
     return Action::None;
+}
+
+Action ChaseAlgorithm::findPathToEnemy(
+  const GameBoard& gameBoard,
+  const Tank& myTank,
+  const Tank& enemyTank
+) {
+  // TODO: Will be implemented in a later commit
+  return Action::None;
+}
+
+ChaseAlgorithm::PathNode* ChaseAlgorithm::runBFS(
+  const GameBoard& gameBoard,
+  PathNode* startNode,
+  const Point& targetPosition,
+  int maxNodes
+) {
+  // TODO: Will be implemented in a later commit
+  return nullptr;
+}
+
+std::vector<ChaseAlgorithm::PathNode*> ChaseAlgorithm::getNextMoves(
+  const GameBoard& gameBoard,
+  const PathNode* currentNode
+) {
+  // TODO: Will be implemented in a later commit
+  return std::vector<PathNode*>();
+}
+
+bool ChaseAlgorithm::isSameState(
+  const std::tuple<Point, Direction, bool, int>& pos1,
+  const std::tuple<Point, Direction, bool, int>& pos2
+) const {
+  // TODO: Will be implemented in a later commit
+  return false;
+}
+
+Action ChaseAlgorithm::extractFirstAction(const PathNode* endNode) const {
+  // TODO: Will be implemented in a later commit
+  return Action::None;
+}
+
+void ChaseAlgorithm::cleanupNodes(const std::vector<PathNode*>& nodes) {
+  // TODO: Will be implemented in a later commit
 }
