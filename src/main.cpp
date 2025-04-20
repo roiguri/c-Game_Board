5c4707a185b33a349fc40add0adb331373568931
roiguri <155663297+roiguri@users.noreply.github.com>
2025-04-21 00:12:41 +0300

    Add command line arguments to main

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,27 @@
-#include <iostream>
 #include "game_manager.h"
 #include <iostream>
 #include <string>
+#include <filesystem>
 
-int main() {
-  // Path to the board configuration file
-  const std::string boardFilePath = "board.txt";
-  const std::string outputFilePath = "game_results.txt";
+int main(int argc, char* argv[]) {
+  if (argc != 2) {
+    std::cerr << "Usage: " << argv[0] << " <game_board_input_file>" << std::endl;
+    return 1;
+  }
+
+  const std::string inputFilePath = argv[1];
   GameManager gameManager;
 
-  if (!gameManager.initialize(boardFilePath)) {
-      std::cerr << "Failed to initialize the game." << std::endl;
+  if (!gameManager.initialize(inputFilePath)) {
       return 1;
   }
+  
   gameManager.runGame();
+  
+  // TODO: do we need a specific file name for the output?
+  const std::string outputFilePath = "game_results.txt";
   gameManager.saveResults(outputFilePath);
+
   std::cout << "Game completed successfully. Results saved to " << outputFilePath << "." << std::endl;
   return 0;
 }
