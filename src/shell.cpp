e83e0b4df63c50ef1a467c0ca272543b5f975875
roiguri <155663297+roiguri@users.noreply.github.com>
2025-04-24 20:00:25 +0300

    Update Shell class to use base gameObject class

--- src/shell.cpp
+++ src/shell.cpp
@@ -1,48 +1,7 @@
 #include "shell.h"
 
 Shell::Shell(int playerId, const Point& position, Direction direction)
-    : m_playerId(playerId), 
-      m_position(position),
-      m_previousPosition(position), 
-      m_direction(direction),
-      m_destroyed(false) {
-}
-
-Shell::~Shell() {
-    // No dynamic resources to clean up
-}
-
-int Shell::getPlayerId() const {
-  return m_playerId;
-}
-
-Point Shell::getPosition() const {
-  return m_position;
-}
-
-Point Shell::getPreviousPosition() const {
-  return m_previousPosition;
-}
-
-void Shell::updatePreviousPosition() {
-  m_previousPosition = m_position;
-}
-
-Direction Shell::getDirection() const {
-  return m_direction;
-}
-
-bool Shell::isDestroyed() const {
-  return m_destroyed;
-}
-
-void Shell::setPosition(const Point& position) {
-  m_previousPosition = m_position;
-  m_position = position;
-}
-
-void Shell::destroy() {
-  m_destroyed = true;
+    : GameObject(playerId, position, direction) {
 }
 
 Point Shell::getNextPosition() const {
