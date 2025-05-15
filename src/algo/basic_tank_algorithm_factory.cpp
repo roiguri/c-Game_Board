231d9a45377304f809301e1dfcc47813186e7c75
Roi Guri <roiguri@gmail.com>
2025-05-15 12:01:21 +0300

    Add update state to offensive algorithm

--- src/algo/basic_tank_algorithm_factory.cpp
+++ src/algo/basic_tank_algorithm_factory.cpp
@@ -1,10 +1,13 @@
 #include "algo/basic_tank_algorithm_factory.h"
 #include "algo/basic_tank_algorithm.h"
+#include "algo/offensive_tank_algorithm.h"
 
 std::unique_ptr<TankAlgorithm> BasicTankAlgorithmFactory::create(
     int player_index, 
     int tank_index) const {
-    
-    // Create and return a new BasicTankAlgorithm instance
-    return std::make_unique<BasicTankAlgorithm>(player_index, tank_index);
+    if (player_index == 1) {
+        return std::make_unique<BasicTankAlgorithm>(player_index, tank_index);
+    } else {
+        return std::make_unique<OffensiveTankAlgorithm>(player_index, tank_index);
+    }
 }
--- test/algo/basic_tank_algorithm_factory_test.cpp
+++ test/algo/basic_tank_algorithm_factory_test.cpp
@@ -1,6 +1,7 @@
 #include "gtest/gtest.h"
 #include "algo/basic_tank_algorithm_factory.h"
 #include "algo/basic_tank_algorithm.h"
+#include "algo/offensive_tank_algorithm.h"
 #include <memory>
 
 TEST(BasicTankAlgorithmFactoryTest, CreatesBasicTankAlgorithm) {
@@ -12,4 +13,15 @@ TEST(BasicTankAlgorithmFactoryTest, CreatesBasicTankAlgorithm) {
     // Check type via dynamic_cast
     auto* basicAlgo = dynamic_cast<BasicTankAlgorithm*>(algo.get());
     ASSERT_NE(basicAlgo, nullptr);
+} 
+
+TEST(BasicTankAlgorithmFactoryTest, CreatesOffensiveTankAlgorithm) {
+    BasicTankAlgorithmFactory factory;
+    int playerIndex = 2;
+    int tankIndex = 2;
+    std::unique_ptr<TankAlgorithm> algo = factory.create(playerIndex, tankIndex);
+    ASSERT_NE(algo, nullptr);
+    // Check type via dynamic_cast
+    auto* offensiveAlgo = dynamic_cast<OffensiveTankAlgorithm*>(algo.get());
+    ASSERT_NE(offensiveAlgo, nullptr);
 } 
