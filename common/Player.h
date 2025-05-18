09e4893e15f9fc549470d3d7b3c3ee0bbe910ab5
Roi Guri <roiguri@gmail.com>
2025-05-18 12:55:55 +0300

    Temporary solution for player constructor

--- common/Player.h
+++ common/Player.h
@@ -24,7 +24,13 @@ public:
      * @param num_shells Number of shells per tank at game start
      */  
     Player(int player_index, size_t x, size_t y, size_t max_steps,
-                                                 size_t num_shells) {}
+                                                 size_t num_shells) {
+                                                    (void)player_index;
+                                                    (void)x;
+                                                    (void)y;
+                                                    (void)max_steps;
+                                                    (void)num_shells;
+                                                 }
                                                  
     /**
      * @brief Virtual destructor
