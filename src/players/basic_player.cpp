a46812b88225b01542dd36b8511760e356004524
Roi Guri <roiguri@gmail.com>
2025-06-02 13:44:24 +0300

    players

--- src/players/basic_player.cpp
+++ src/players/basic_player.cpp
@@ -3,29 +3,30 @@
 #include <cassert>
 #include <iostream>
 
-BasicPlayer::BasicPlayer(int player_index, size_t x, size_t y, size_t max_steps, size_t num_shells)
-    : Player(player_index, x, y, max_steps, num_shells),
-      m_player_index(player_index),
-      m_board_width(x),
-      m_board_height(y),
-      m_max_steps(max_steps),
-      m_num_shells(num_shells),
+BasicPlayer::BasicPlayer(int playerIndex, size_t x, size_t y, size_t maxSteps, size_t numShells)
+    : Player(playerIndex, x, y, maxSteps, numShells),
+      m_playerIndex(playerIndex),
+      m_boardWidth(x),
+      m_boardHeight(y),
+      m_maxSteps(maxSteps),
+      m_numShells(numShells),
+      // Cast to int as BattleInfoImpl constructor expects int parameters for board dimensions
       m_battleInfo(static_cast<int>(x), static_cast<int>(y)) {}
 
 BasicPlayer::~BasicPlayer() = default;
 
-void BasicPlayer::updateTankWithBattleInfo(TankAlgorithm& tank, SatelliteView& satellite_view) {
+void BasicPlayer::updateTankWithBattleInfo(TankAlgorithm& tank, SatelliteView& satelliteView) {
     // 1. Populate battle info from satellite view
-    populateBattleInfo(satellite_view);
+    populateBattleInfo(satelliteView);
     // 2. Pass to tank algorithm
     tank.updateBattleInfo(m_battleInfo);
 }
 
-void BasicPlayer::populateBattleInfo(SatelliteView& satellite_view) {
+void BasicPlayer::populateBattleInfo(SatelliteView& satelliteView) {
     m_battleInfo.clear();
-    for (size_t y = 0; y < m_board_height; ++y) {
-        for (size_t x = 0; x < m_board_width; ++x) {
-            char obj = satellite_view.getObjectAt(x, y);
+    for (size_t y = 0; y < m_boardHeight; ++y) {
+        for (size_t x = 0; x < m_boardWidth; ++x) {
+            char obj = satelliteView.getObjectAt(x, y);
             GameBoard::CellType cellType = GameBoard::CellType::Empty;
             switch (obj) {
                 case '#':
@@ -45,7 +46,7 @@ void BasicPlayer::populateBattleInfo(SatelliteView& satellite_view) {
                 case '1': case '2': case '3': case '4': case '5': case '6': case '7': case '8': case '9': {
                     cellType = GameBoard::CellType::Empty;
                     int tankPlayerId = obj - '0';
-                    if (tankPlayerId == m_player_index) {
+                    if (tankPlayerId == m_playerIndex) {
                         m_battleInfo.addFriendlyTankPosition(Point(x, y));
                     } else {
                         m_battleInfo.addEnemyTankPosition(Point(x, y));
