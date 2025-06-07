73c6a57d9419d0367cb1aa669def67727faca9a2
Roi Guri <roiguri@gmail.com>
2025-06-07 12:28:32 +0300

    Refactor BasicPlayer and TankAlgorithm logic
    - Simplified the population of battle info in BasicPlayer by removing unnecessary clearing of m_battleInfo.
    - Updated BasicTankAlgorithm to streamline action requests, ensuring that the action to move to a safe position is set as default.
    - Enhanced safety checks in OffensiveTankAlgorithm for the first step from the start position and improved action handling logic.

--- src/players/basic/basic_player.cpp
+++ src/players/basic/basic_player.cpp
@@ -23,11 +23,10 @@ void BasicPlayer::updateTankWithBattleInfo(TankAlgorithm& tank, SatelliteView& s
 }
 
 void BasicPlayer::populateBattleInfo(SatelliteView& satelliteView) {
-    m_battleInfo.clear();
     for (size_t y = 0; y < m_boardHeight; ++y) {
         for (size_t x = 0; x < m_boardWidth; ++x) {
             char obj = satelliteView.getObjectAt(x, y);
-            GameBoard::CellType cellType = GameBoard::CellType::Empty;
+            GameBoard::CellType cellType = m_battleInfo.getCellType(x, y);
             switch (obj) {
                 case '#':
                     cellType = GameBoard::CellType::Wall;
@@ -40,7 +39,6 @@ void BasicPlayer::populateBattleInfo(SatelliteView& satelliteView) {
                     m_battleInfo.setOwnTankPosition(Point(x, y));
                     break;
                 case '*':
-                    cellType = GameBoard::CellType::Empty;
                     m_battleInfo.addShellPosition(Point(x, y));
                     break;
                 case '1': case '2': case '3': case '4': case '5': case '6': case '7': case '8': case '9': {
