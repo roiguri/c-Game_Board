12d4c1143800fd460cb4440e06d7d39db0a0186c
Roi Guri <roiguri@gmail.com>
2025-06-26 13:01:31 +0300

    fix: match common interface

--- common/AbstractGameManager.h
+++ common/AbstractGameManager.h
@@ -14,11 +14,11 @@ public:
     virtual ~AbstractGameManager() {}
     virtual GameResult run(
         size_t map_width, size_t map_height,
-        SatelliteView& map, // <= assume it is a snapshot, NOT updated
+        const SatelliteView& map, // <= a snapshot, NOT updated
         size_t max_steps, size_t num_shells,
         Player& player1, Player& player2,
         TankAlgorithmFactory player1_tank_algo_factory,
         TankAlgorithmFactory player2_tank_algo_factory) = 0;
 };
 
-using GameManagerFactory = std::function<std::unique_ptr<AbstractGameManager>()>;
+using GameManagerFactory = std::function<std::unique_ptr<AbstractGameManager>(bool verbose)>;
